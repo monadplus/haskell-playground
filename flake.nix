c448e44056cfd7daef4af995ec7ee83f50151bd0
Arnau Abella <arnauabella@gmail.com>
2023-06-03 09:50:44 +0200

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "My haskell application";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        haskellPackages = pkgs.haskellPackages;
+
+        jailbreakUnbreak = pkg:
+          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
+
+        packageName = "playground";
+      in {
+        packages.${packageName} =
+          haskellPackages.callCabal2nix "cabal/$packageName" (self + "/cabal") rec {
+            transformers = haskellPackages.transformers_0_6_1_0;
+          };
+
+        packages.default = self.packages.${system}.${packageName};
+        defaultPackage = self.packages.${system}.default;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cabal-install
+            haskellPackages.haskell-language-server
+            ghcid
+          ];
+          inputsFrom = map (__getAttr "env") (__attrValues self.packages.${system});
+        };
+        devShell = self.devShells.${system}.default;
+      });
+}
